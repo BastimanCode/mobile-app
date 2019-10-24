d32eac575ec727d9f57463a700c854f75ed91689
Felix <Felix1Tanner@googlemail.com>
2019-10-24 12:30:49 +0200

    Test project server client communication android desktop

---
+++ MeinProjekt/app/src/main/java/com/example/meinprojekt/HalloActivity.java
@@ -0,0 +1,14 @@
+package com.example.meinprojekt;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class HalloActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hallo);
+    }
+}
