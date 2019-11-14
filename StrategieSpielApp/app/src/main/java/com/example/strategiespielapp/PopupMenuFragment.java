3f0c20792d707a67bd6f281fa1fb55c078000804
Bastian Schneider <bschneider@fh-bielefeld.de>
2019-11-14 16:51:06 +0100

    Menüfragment in allen Activitys

---
+++ StrategieSpielApp/app/src/main/java/com/example/strategiespielapp/PopupMenuFragment.java
@@ -0,0 +1,111 @@
+package com.example.strategiespielapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.PopupMenu;
+
+public class PopupMenuFragment extends Fragment implements PopupMenu.OnMenuItemClickListener, View.OnClickListener {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View v =  inflater.inflate(R.layout.fragment_popup_menufragment, container, false);
+
+        Button b = (Button) v.findViewById(R.id.menu_button);
+        b.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onClick(View view) {
+        showPopup(view);
+    }
+
+    public void showPopup(View view) {
+        PopupMenu popup = new PopupMenu(getActivity(), view);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.navigation_menu);
+        popup.show();
+    }
+
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.resources:
+                Intent resourcesIntent = new Intent(getActivity(), ResourcesActivity.class);
+                startActivity(resourcesIntent);
+                return true;
+            case R.id.buildings:
+                Intent buildingsIntent = new Intent(getActivity(), BuildingsActivity.class);
+                startActivity(buildingsIntent);
+                return true;
+            case R.id.research:
+                Intent researchIntent = new Intent(getActivity(), ResearchActivity.class);
+                startActivity(researchIntent);
+                return true;
+            case R.id.units:
+                Intent unitsIntent = new Intent(getActivity(), UnitsActivity.class);
+                startActivity(unitsIntent);
+                return true;
+            case R.id.fleets:
+                Intent fleetsIntent = new Intent(getActivity(), FleetsActivity.class);
+                startActivity(fleetsIntent);
+                return true;
+            case R.id.galaxy:
+                Intent galaxyIntent = new Intent(getActivity(), GalaxyActivity.class);
+                startActivity(galaxyIntent);
+                return true;
+            default:
+                return false;
+        }
+    }
+
+/*    public void showPopupShort() {
+        View menuItemView = getView().findViewById(R.id.menu_button);
+        PopupMenu popupMenu = new PopupMenu(getActivity(), menuItemView);
+        popupMenu.getMenuInflater().inflate(R.menu.navigation_menu, popupMenu.getMenu());
+
+        popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.resources:
+                        Intent resourcesIntent = new Intent(getActivity(), ResourcesActivity.class);
+                        startActivity(resourcesIntent);
+                        return true;
+                    case R.id.buildings:
+                        Intent buildingsIntent = new Intent(getActivity(), BuildingsActivity.class);
+                        startActivity(buildingsIntent);
+                        return true;
+                    case R.id.research:
+                        Intent researchIntent = new Intent(getActivity(), ResearchActivity.class);
+                        startActivity(researchIntent);
+                        return true;
+                    case R.id.units:
+                        Intent unitsIntent = new Intent(getActivity(), UnitsActivity.class);
+                        startActivity(unitsIntent);
+                        return true;
+                    case R.id.fleets:
+                        Intent fleetsIntent = new Intent(getActivity(), FleetsActivity.class);
+                        startActivity(fleetsIntent);
+                        return true;
+                    case R.id.galaxy:
+                        Intent galaxyIntent = new Intent(getActivity(), GalaxyActivity.class);
+                        startActivity(galaxyIntent);
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        popupMenu.show();
+    }*/
+}
