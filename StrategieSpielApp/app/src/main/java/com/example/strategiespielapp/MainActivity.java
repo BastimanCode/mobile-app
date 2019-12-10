7924db9b8f5ee027e74b98cfe4c60399ed2fc400
Felix <Felix1Tanner@googlemail.com>
2019-12-10 11:45:32 +0100

    finished overview

--- StrategieSpielApp/app/src/main/java/com/example/strategiespielapp/MainActivity.java
+++ StrategieSpielApp/app/src/main/java/com/example/strategiespielapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.strategiespielapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.os.Bundle;
 
 
@@ -10,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
